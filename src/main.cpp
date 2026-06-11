3a41582ac0f032726533ff37d8762c0ba373b9b0
hieuvh <hieuvh@crownx.masangroup.com>
2026-06-11 17:17:39 +0700

    Init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+
+#include "stdio.h"
+#include "iostream"
+#include "windows.h"
+#include "sql.h"
+#include "sqltypes.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello world!";
+    return 0;
+}
